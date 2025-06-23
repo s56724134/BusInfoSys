2dcc334efa4fbbbc1ebd374e73750322b97033dc
s56724134 <psg111444@gmail.com>
2025-06-23 03:48:28 +0000

    feat: add ToRemindDto Method

--- api/Mapper/RemindMapper.cs
+++ api/Mapper/RemindMapper.cs
@@ -2,17 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Remind;
+using api.Models.LineModel;
 
 namespace api.Mapper
 {
     public static class RemindMapper
     {
-        // public static Remind ToRemindFromCreateDto(this CreateRemindRequestDto createDto)
-        // {
-        //     return new Remind
-        //     {
-        //         userId =
-        //     }
-        // }
+        public static RemindDto ToRemindDto(this Remind remind)
+        {
+            return new RemindDto()
+            {
+                Id = remind.Id,
+                StopName = remind.Stop?.StopName ?? "",
+                RouteName = remind.Stop?.Bus?.Route ?? "",
+                RemindTime = remind.RemindTime != null ? new RemindTimeValueDto()
+                {
+                    SelectedRemindTime = remind.RemindTime.SelectedRemindTime,
+                    RepeatWeekTime = remind.RemindTime.RepeatWeekTime ?? new List<string>(),
+                    BusShowUpTime = new BusShowUpTimeDto()
+                    {
+                        Start = remind.RemindTime.BusShowUpTime?.Start ?? "",
+                        End = remind.RemindTime.BusShowUpTime?.End ?? ""
+                    }
+                } : null
+            };
+        }
     }
 }
